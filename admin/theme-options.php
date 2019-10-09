048d575216881fca154b09abcd064fdb8c0966a4
KAYGB <kaygbtop@163.com>
2019-10-09 21:25:46 +0800

    修复后台框架bug

--- admin/theme-options.php
+++ admin/theme-options.php
@@ -401,7 +401,99 @@ switch ( $value['type'] ) {
 ?>
 <!-- 结束建立选项类型 -->
 <div class="panel" id="about_theme">
-<?php include_once(theme-version.php);?>
+<?php //require(easeversion.php);?>
+<h2>Ease主题 </h2>
+    <b>version: 1.4.2</b>
+    <p>Ease的英文意思：容易；安逸；自在；舒适
+        <br>
+        目的是创造出一个简单，快速，好看，用起来舒适的主题
+    </p>
+    <p>使用教程请访问<a href="https://kaygb.top">未来小趣</a></p>
+    <hr>
+<h2>更新日志</h2>
+<hr>
+
+<p>
+<b>Ease 1.4.2</b>
+        <br>
+        2019-10-09
+        <ul>
+            <li>添加时间线文章归档页面</li>
+            <li>后台框架bug修复</li>
+            <li>css模块化重构（未完成）</li>
+        </ul>
+</p>
+<hr>
+
+<p>
+<b>Ease 1.4</b>
+        <br>
+        2019-10-07
+        <ul>
+            <li>优化并适配移动端导航栏自适应</li>
+            <li>添加标签云页面，需要自行添加模板</li>
+            <li>添加独立友情链接页面，并且添加自助申请友链</li>
+            <li>美化文章分类和发布时间</li>
+            <li>美化标题栏</li>
+            <li>添加返回顶部按钮</li>
+            <li>css模块化重构（未完成）</li>
+        </ul>
+</p>
+<hr>
+
+<p>
+<b>Ease 1.3</b>
+        <br>
+        2019-08-19
+        <ul>
+            <li>美化导航栏</li>
+            <li>修复文章内区块显示错位的问题</li>
+            <li>添加文章翻页</li>
+            <li>优化404页面</li>
+            <li>修复SEO问题</li>
+        </ul>
+</p>
+<hr>
+
+
+<p>
+<b>Ease 1.2</b>
+        <br>
+        2019-08-18
+        <ul>
+            <li>添加简单SEO设置</li>
+            <li>删除侧边栏</li>
+            <li>优化评论模块</li>
+        </ul>
+</p>
+<hr>
+
+
+<p>
+<b>Ease 1.1</b>
+        <br>
+        2019-08-02
+        <ul>
+            <li></li>
+            <li>添加后台主题框架</li>
+            <li></li>
+        </ul>
+</p>
+<hr>
+
+
+    <p>
+        <b>Ease 1.0</b>
+        <br>
+        2019-08-02
+        <ul>
+            <li>初始版本</li>
+            <li>构建基本框架</li>
+            <li>构建基本样式</li>
+        </ul>
+        
+</p>
+    <hr>
 
 </div>
 <p class="submit">
