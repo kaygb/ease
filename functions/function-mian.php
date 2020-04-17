af17a40efa376ae2e55ac80f3d6a11a888a01d03
QingShanKing <qingshanking@vip.qq.com>
2020-04-17 08:53:15 +0800

    提升后台速度
    提升后台速度

--- functions/function-mian.php
+++ functions/function-mian.php
@@ -23,3 +23,21 @@ add_filter('pre_option_link_manager_enabled', '__return_true');
 
 // 开启文章缩略图
 add_theme_support('post-thumbnails');
+
+
+// 禁止后台加载谷歌字体
+function wp_remove_open_sans_from_wp_core() {
+	wp_deregister_style( 'open-sans' );
+	wp_register_style( 'open-sans', false );
+	wp_enqueue_style('open-sans','');
+}
+add_action( 'init', 'wp_remove_open_sans_from_wp_core' );
+
+// 禁用emoji
+function disable_emojis() {
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+}
+add_action( 'init', 'disable_emojis' );
+
+// 移除表情
+remove_action( 'wp_head' , 'print_emoji_detection_script', 7 );
